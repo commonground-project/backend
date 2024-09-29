bd331f85928d109317ff0c23b7b5d5f0c4a563c7
Touyu Pan <firward84081441@gmail.com>
2024-09-29 15:56:24 +0800

    build: initial backend repository

---
+++ src/test/java/com/example/CommonGroundBackend/CommonGroundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommonGroundBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonGroundBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
