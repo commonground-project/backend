3c4f46cd572dba3ca7a0da16e91dc0d5257c466e
Touyu Pan <firward84081441@gmail.com>
2024-10-03 22:14:17 +0800

    chore: build CI

--- src/main/java/tw/commonground/backend/BackendApplication.java
+++ src/main/java/tw/commonground/backend/BackendApplication.java
@@ -2,12 +2,20 @@ package tw.commonground.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
 }
