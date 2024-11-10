0676181b6701672bb35a14040196a4fe457cb6a1
Kyle9410-Chen <kyle941010@gmail.com>
2024-11-10 18:26:43 +0800

    fix: add missing file

---
+++ src/main/java/tw/commonground/backend/service/fact/FactEntity.java
@@ -0,0 +1,46 @@
+package tw.commonground.backend.service.fact;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Fact")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class FactEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @CreatedDate
+    @Column(nullable=false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @Column(nullable=false)
+    private String title;
+
+    // TODO: Waiting User Entity
+    @Column(nullable=false)
+    private Long authorId;
+
+    @Column(nullable=false)
+    private String authorName;
+
+}
