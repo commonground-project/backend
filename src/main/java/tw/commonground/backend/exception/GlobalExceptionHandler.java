ee5ec0df72089934d6e9b183e030acd8bf0a0e48
mochizuki <1p41p4jejo@gmail.com>
2024-11-20 07:09:06 +0800

    feat: add exception handle for resource not found and method not allow

--- src/main/java/tw/commonground/backend/exception/GlobalExceptionHandler.java
+++ src/main/java/tw/commonground/backend/exception/GlobalExceptionHandler.java
@@ -6,16 +6,44 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.ErrorResponseException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.net.URI;
 
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(@NonNull NoResourceFoundException ex,
+                                                                    @NonNull HttpHeaders headers,
+                                                                    @NonNull HttpStatusCode status,
+                                                                    @NonNull WebRequest request) {
+        ErrorResponseException exception = new ErrorResponseException(status);
+        exception.setTitle("Not found");
+        exception.setType(URI.create("type:NOT_FOUND"));
+        exception.setDetail("The requested resource was not found.");
+
+        return super.handleExceptionInternal(exception, null, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(@NonNull HttpRequestMethodNotSupportedException ex,
+                                                                         @NonNull HttpHeaders headers,
+                                                                         @NonNull HttpStatusCode status,
+                                                                         @NonNull WebRequest request) {
+        ErrorResponseException exception = new ErrorResponseException(status);
+        exception.setTitle("Method not supported");
+        exception.setType(URI.create("type:METHOD_NOT_SUPPORTED"));
+        exception.setDetail("The requested method is not supported for this resource.");
+
+        return super.handleExceptionInternal(exception, null, headers, status, request);
+    }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
