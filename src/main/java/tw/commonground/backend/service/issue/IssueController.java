fa5913b771189397a504a7828082c5f02c595feb
mochizuki <1p41p4jejo@gmail.com>
2024-12-25 14:44:50 +0800

    style: fix coding style issue

--- src/main/java/tw/commonground/backend/service/issue/IssueController.java
+++ src/main/java/tw/commonground/backend/service/issue/IssueController.java
@@ -3,6 +3,7 @@ package tw.commonground.backend.service.issue;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import tw.commonground.backend.service.fact.FactService;
@@ -58,35 +59,38 @@ public class IssueController {
     }
 
     @PostMapping("/issues")
-    public IssueResponse createIssue(@AuthenticationPrincipal FullUserEntity user,
-                                     @Valid @RequestBody IssueRequest issueRequest) {
+    public ResponseEntity<IssueResponse> createIssue(@AuthenticationPrincipal FullUserEntity user,
+                                                     @Valid @RequestBody IssueRequest issueRequest) {
 
         IssueEntity issueEntity = issueService.createIssue(issueRequest, user);
         ContentContainFact contentContainFact = ContentContainFactParser
                 .separateContentAndFacts(issueEntity.getInsight());
 
         List<FactEntity> factResponses = factService.getFacts(contentContainFact.getFacts());
-        return IssueMapper.toResponse(issueEntity, factResponses);
+        IssueResponse response = IssueMapper.toResponse(issueEntity, factResponses);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/issue/{id}")
-    public IssueResponse getIssue(@PathVariable UUID id) {
+    public ResponseEntity<IssueResponse> getIssue(@PathVariable UUID id) {
         IssueEntity issueEntity = issueService.getIssue(id);
         ContentContainFact contentContainFact = ContentContainFactParser
                 .separateContentAndFacts(issueEntity.getInsight());
 
         List<FactEntity> factResponses = factService.getFacts(contentContainFact.getFacts());
-        return IssueMapper.toResponse(issueEntity, factResponses);
+        IssueResponse response = IssueMapper.toResponse(issueEntity, factResponses);
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/issue/{id}")
-    public IssueResponse updateIssue(@PathVariable UUID id, @Valid @RequestBody IssueRequest issueRequest) {
+    public ResponseEntity<IssueResponse> updateIssue(@PathVariable UUID id, @Valid @RequestBody IssueRequest issueRequest) {
         IssueEntity issueEntity = issueService.updateIssue(id, issueRequest);
         ContentContainFact contentContainFact = ContentContainFactParser
                 .separateContentAndFacts(issueEntity.getInsight());
 
         List<FactEntity> factResponses = factService.getFacts(contentContainFact.getFacts());
-        return IssueMapper.toResponse(issueEntity, factResponses);
+        IssueResponse response = IssueMapper.toResponse(issueEntity, factResponses);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/issue/{id}")
