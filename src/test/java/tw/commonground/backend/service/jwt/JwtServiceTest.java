56e4b046031d4dd6885ee8de57108c7046f8bf38
mochizuki <1p41p4jejo@gmail.com>
2024-12-29 03:33:43 +0800

    style: fix coding style issue

--- src/test/java/tw/commonground/backend/service/jwt/JwtServiceTest.java
+++ src/test/java/tw/commonground/backend/service/jwt/JwtServiceTest.java
@@ -20,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.Optional;
 import java.util.UUID;
 
+@SuppressWarnings("MethodName")
 @ExtendWith(MockitoExtension.class)
 class JwtServiceTest {
 
