fe36e394b0774eb8bb46c52b210f06ba9a9cbbcd
kiki <lioukiki@gmail.com>
2025-01-01 15:48:34 +0800

    fix: Debugging ViewpointRepositoryTest for id regenerate and flush problem

--- src/test/java/tw/commonground/backend/service/viewpoint/ViewpointRepositoryTest.java
+++ src/test/java/tw/commonground/backend/service/viewpoint/ViewpointRepositoryTest.java
@@ -1,10 +1,13 @@
 package tw.commonground.backend.service.viewpoint;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
+import tw.commonground.backend.exception.EntityNotFoundException;
 import tw.commonground.backend.service.viewpoint.entity.Reaction;
 import tw.commonground.backend.service.viewpoint.entity.ViewpointEntity;
 import tw.commonground.backend.service.viewpoint.entity.ViewpointRepository;
@@ -17,18 +20,22 @@ public class ViewpointRepositoryTest {
     @Autowired
     private ViewpointRepository viewpointRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
     @Test
     @Transactional
     void testUpdateReactionCount() {
-
-        UUID viewpointId = UUID.randomUUID();
         ViewpointEntity viewpointEntity = new ViewpointEntity();
-        viewpointEntity.setId(viewpointId);
+        viewpointEntity.setTitle("Test viewpoint");
         viewpointRepository.save(viewpointEntity);
+        UUID viewpointId = viewpointEntity.getId(); // Get the generated UUID
 
         viewpointRepository.updateReactionCount(viewpointId, Reaction.LIKE, 1);
-
-        ViewpointEntity updatedViewpoint = viewpointRepository.findById(viewpointId).orElseThrow();
+        entityManager.clear();
+        ViewpointEntity updatedViewpoint = viewpointRepository.findById(viewpointId).orElseThrow(
+                () -> new EntityNotFoundException("Viewpoint", "id", viewpointId.toString())
+        );
         assertEquals(1, updatedViewpoint.getLikeCount());
     }
 }
