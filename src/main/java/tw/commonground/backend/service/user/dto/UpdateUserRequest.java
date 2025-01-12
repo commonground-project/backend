c280e01e95d9d6ade45c568a05eab862fedf6453
mochizuki <1p41p4jejo@gmail.com>
2025-01-12 13:49:29 +0800

    feat: add update user endpoint

---
+++ src/main/java/tw/commonground/backend/service/user/dto/UpdateUserRequest.java
@@ -0,0 +1,25 @@
+package tw.commonground.backend.service.user.dto;
+
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.lang.Nullable;
+import tw.commonground.backend.service.user.entity.UserRole;
+
+@Getter
+@Setter
+public class UpdateUserRequest {
+
+    @Nullable
+    @Size(min = 4, max = 15, message = "Username must be between 4 and 15 characters long")
+    @Pattern(regexp = "^\\w+$", message = "Username can only contain letters, numbers, and underscores")
+    private String username;
+
+    @Nullable
+    @Size(min = 1, max = 20, message = "Nickname must be between 1 and 20 characters long")
+    private String nickname;
+
+    @Nullable
+    private UserRole role;
+}
