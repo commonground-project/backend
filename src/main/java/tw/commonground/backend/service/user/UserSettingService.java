d72597a66cf75f05b81536d423dd114be398eb9e
mochizuki <1p41p4jejo@gmail.com>
2025-01-29 14:24:12 +0800

    fix: fix null id in user settings

--- src/main/java/tw/commonground/backend/service/user/UserSettingService.java
+++ src/main/java/tw/commonground/backend/service/user/UserSettingService.java
@@ -1,7 +1,10 @@
 package tw.commonground.backend.service.user;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
 import tw.commonground.backend.service.user.dto.UserSettingDto;
+import tw.commonground.backend.service.user.entity.UserEntity;
 import tw.commonground.backend.service.user.entity.UserSettingEntity;
 import tw.commonground.backend.service.user.entity.UserSettingRepository;
 
@@ -10,23 +13,30 @@ public class UserSettingService {
 
     private final UserSettingRepository userSettingRepository;
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     public UserSettingService(UserSettingRepository userSettingRepository) {
         this.userSettingRepository = userSettingRepository;
     }
 
     public UserSettingEntity getUserSetting(Long userId) {
+        UserEntity user = entityManager.getReference(UserEntity.class, userId);
+
         return userSettingRepository.findById(userId)
                 .orElseGet(() -> {
                     UserSettingEntity newSetting = UserSettingEntity.builder()
-                            .id(userId)
                             .newReplyInMyViewpoint(true)
                             .newReferenceToMyReply(true)
+                            .user(user)
                             .build();
                     return userSettingRepository.save(newSetting);
                 });
     }
 
     public UserSettingEntity updateUserSetting(Long userId, UserSettingDto userSettingDto) {
+        UserEntity user = entityManager.getReference(UserEntity.class, userId);
+
         userSettingRepository.findById(userId)
                 .ifPresentOrElse(userSettingEntity -> {
                     userSettingEntity.setNewReplyInMyViewpoint(userSettingDto.isNewReplyInMyViewpoint());
@@ -34,9 +44,9 @@ public class UserSettingService {
                     userSettingRepository.save(userSettingEntity);
                 }, () -> {
                     UserSettingEntity newSetting = UserSettingEntity.builder()
-                            .id(userId)
                             .newReplyInMyViewpoint(userSettingDto.isNewReplyInMyViewpoint())
                             .newReferenceToMyReply(userSettingDto.isNewReferenceToMyReply())
+                            .user(user)
                             .build();
                     userSettingRepository.save(newSetting);
                 });
