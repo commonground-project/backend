4614587fac161ca49bbd2bb28b8e81c1bf29ba0e
kiki <lioukiki@gmail.com>
2025-03-20 15:40:46 +0800

    fix: add the JsonProperty annotation to properties in FallbackResponse to fit in the data format

--- src/main/java/tw/commonground/backend/service/reference/dto/FallbackResponse.java
+++ src/main/java/tw/commonground/backend/service/reference/dto/FallbackResponse.java
@@ -1,6 +1,7 @@
 package tw.commonground.backend.service.reference.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,13 +12,10 @@ import lombok.ToString;
 @ToString
 @AllArgsConstructor
 public class FallbackResponse {
+    @JsonProperty("url")
+    private String url;
+
+    @JsonProperty("title")
     private String title;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
