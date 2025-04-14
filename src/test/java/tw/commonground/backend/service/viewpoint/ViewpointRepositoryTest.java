922a640a6ea9b07b4dd426ffcbb8164df5f6cbcf
kiki <lioukiki@gmail.com>
2025-04-14 15:08:05 +0800

    fix: fix the wrong import for REACTION

--- src/test/java/tw/commonground/backend/service/viewpoint/ViewpointRepositoryTest.java
+++ src/test/java/tw/commonground/backend/service/viewpoint/ViewpointRepositoryTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 import tw.commonground.backend.exception.EntityNotFoundException;
-import tw.commonground.backend.service.viewpoint.entity.Reaction;
+import tw.commonground.backend.shared.entity.Reaction;
 import tw.commonground.backend.service.viewpoint.entity.ViewpointEntity;
 import tw.commonground.backend.service.viewpoint.entity.ViewpointRepository;
 
