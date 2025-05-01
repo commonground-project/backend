8d9092a9d8db44d33efde2a46a54b1803d48a4fc
yichen <sunny930817@gmail.com>
2025-05-01 16:14:44 +0800

    fix: specify @JoinTable to match existing fact_entity_references table

--- src/main/java/tw/commonground/backend/service/fact/entity/FactEntity.java
+++ src/main/java/tw/commonground/backend/service/fact/entity/FactEntity.java
@@ -40,6 +40,11 @@ public class FactEntity extends BaseEntityWithAuthor {
     private String title;
 
     @ManyToMany
+    @JoinTable(
+            name = "fact_entity_references",
+            joinColumns = @JoinColumn(name = "facts_id"),
+            inverseJoinColumns = @JoinColumn(name = "references_id")
+    )
     @ToString.Exclude
     private Set<ReferenceEntity> references;
 }
