92ba0ef44fc7d5d8c53182df00f13f49664bb674
Umineko <119949629+Kyle9410-Chen@users.noreply.github.com>
2025-05-01 21:48:31 +0800

    Revert "Add fact search service"

--- src/main/java/tw/commonground/backend/exception/SearchServiceException.java
+++
@@ -1,7 +0,0 @@
-package tw.commonground.backend.exception;
-
-public class SearchServiceException extends RuntimeException {
-    public SearchServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
