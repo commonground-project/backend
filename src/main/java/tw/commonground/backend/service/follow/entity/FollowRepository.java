78e7b2e841975defe8605e15cc53d5add51f6613
kiki <lioukiki@gmail.com>
2025-05-01 15:59:16 +0800

    fix: add new line to pass MegaLinter

--- src/main/java/tw/commonground/backend/service/follow/entity/FollowRepository.java
+++ src/main/java/tw/commonground/backend/service/follow/entity/FollowRepository.java
@@ -14,17 +14,19 @@ public interface FollowRepository extends JpaRepository<FollowEntity, FollowKey>
     @Query("select i.follow from FollowEntity i where i.id = :id")
     Optional<Boolean> findFollowById(FollowKey id);
 
-    @Query("select i.user.id from FollowEntity i where i.id.objectId = :objectId " +
-            "and i.id.objectType = :objectType " + "and i.follow = true")
+    @Query("select i.user.id from FollowEntity i where i.id.objectId = :objectId "
+            + "and i.id.objectType = :objectType " + "and i.follow = true")
     Optional<List<Long>> findUsersIdByObjectIdAndFollowTrue(UUID objectId, RelatedObject objectType);
 
 //    @Modifying
 //    @Query(value = "insert into follow_entity (user_id, object_id, object_type, follow, updated_at) "
-//            + "values (:#{#id.userId}, :#{#id.objectId}, :#{#id.objectType}, :follow, current_timestamp)", nativeQuery = true)
+//            + "values (:#{#id.userId}, :#{#id.objectId}, :#{#id.o
+//            bjectType}, :follow, current_timestamp)", nativeQuery = true)
 //    void insertFollowById(FollowKey id, Boolean follow);
 //
 //    @Modifying
 //    @Query(value = "update follow_entity set follow = :follow, updated_at = current_timestamp "
-//            + "where user_id = :#{#id.userId} and object_id = :#{#id.objectId} and object_type = :#{#id.objectType}", nativeQuery = true)
+//            + "where user_id = :#{#id.userId} and object_id = :#{#id.obj
+//            ectId} and object_type = :#{#id.objectType}", nativeQuery = true)
 //    void updateFollowById(FollowKey id, Boolean follow);
 }
