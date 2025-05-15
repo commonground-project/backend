099ba850b485edf281d82b1de6f2bf0196e9d71e
Kyle9410-Chen <kyle941010@gmail.com>
2025-05-16 01:03:18 +0800

    fix: add Redis properties configuration and connection factory

--- src/main/java/tw/commonground/backend/configuration/RedisConfiguration.java
+++ src/main/java/tw/commonground/backend/configuration/RedisConfiguration.java
@@ -3,12 +3,28 @@ package tw.commonground.backend.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 
 @Configuration
 public class RedisConfiguration {
+
+    private final RedisProperties redisProperties;
+
+    public RedisConfiguration(RedisProperties redisProperties) {
+        this.redisProperties = redisProperties;
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisProperties.getHost());
+        config.setPort(redisProperties.getPort());
+        return new LettuceConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
