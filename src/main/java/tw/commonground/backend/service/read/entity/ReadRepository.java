6e381d93aa5c04f58926240328d049c0883b61e6
kiki <lioukiki@gmail.com>
2025-05-17 23:03:48 +0800

    fix: modify query

--- src/main/java/tw/commonground/backend/service/read/entity/ReadRepository.java
+++ src/main/java/tw/commonground/backend/service/read/entity/ReadRepository.java
@@ -1,6 +1,8 @@
 package tw.commonground.backend.service.read.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,5 +13,8 @@ public interface ReadRepository extends JpaRepository<ReadEntity, ReadKey> {
     Optional<ReadEntity> findByIdUserIdAndIdObjectIdAndIdObjectType(
             Long userId, UUID objectId, ReadObjectType objectType);
 
-    List<ReadEntity> findByObjectTypeAndTimestampBefore(ReadObjectType readObjectType, LocalDateTime timestamp);
+
+    @Query("SELECT r FROM ReadEntity r WHERE r.id.objectType = :objectType AND r.timestamp < :timestamp")
+    List<ReadEntity> findByObjectTypeAndTimestampBefore(@Param("objectType") ReadObjectType objectType,
+                                                        @Param("timestamp") LocalDateTime timestamp);
 }
